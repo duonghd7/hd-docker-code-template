1e14e87f82a4a857fd6d8ba75b7f661fd49f8a60
Trist <duonghd1307@gmail.com>
2022-07-19 09:09:26 +0700

    update

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 app.get('/', (req, res) => {
   res.send(
     //`<body style='background-color:#283E5B'><h1 style='color: orange;text-align:center'>Hello AWS ${os.hostname()}</h1></body>`
-    `<body style='background-color:#283E5B'><h1 style='color: orange;text-align:center'>Hello AWS 2</h1></body>`
+    `<body style='background-color:#283E5B'><h1 style='color: orange;text-align:center'>Hello AWS 1111</h1></body>`
   );
 });
 
