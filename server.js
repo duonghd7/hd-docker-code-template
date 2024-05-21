4bff28d065b38719d0456ef4ec36b811f6742c31
Trist <duonghd1307@gmail.com>
2024-05-21 16:35:54 +0700

    refactor: code

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
 'use strict';
 
+require('dotenv').config()
 const express = require('express');
 const os = require('os');
 
 // Constants
-const PORT = 8080;
+const PORT = process.env.APP_PORT;
 const HOST = '0.0.0.0';
 
 // App
 const app = express();
 app.get('/', (req, res) => {
   res.send(
-    `<body style='background-color:#283E5B'><h1 style='color: orange;text-align:center'>Hello AWS - 7 -  ${os.hostname()}</h1></body>`
-    //`<body style='background-color:#283E5B'><h1 style='color: orange;text-align:center'>Hello AWS 1111</h1></body>`
+    `<body style='background-color:#283E5B'><h1 style='color: orange;text-align:center'>${process.env.SECRET_MESSAGE} - ${os.hostname()}</h1></body>`
   );
 });
 
 app.listen(PORT, HOST);
+console.log(`Running with message: ${process.env.SECRET_MESSAGE}`);
 console.log(`Running on http://${HOST}:${PORT}`);
